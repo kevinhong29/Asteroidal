38a4e90dbc03c86d8da152dea25485143cea800a
KKev1229 <kevin122902@gmail.com>
2022-06-23 10:36:53 -0700

    Added some docstrings to class and first method

--- asteroid.py
+++ asteroid.py
@@ -2,23 +2,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astroquery.gaia import Gaia
 import astropy.units as u
+import astropy.table
 from astropy.coordinates import SkyCoord
+import pandas as pd
 
 class Asteroid(object):
+    """
+    An asteroid in Gaia.sso_source database with observations
+
+    Args:
+        source_id (long): Unique source identifier from gaia_source
+        number_mp (long): Minor Planet number attributed by MPC
+        denomination (string): Name of asteroid in MPC database
+
+    Attributes:
+        source_id (long): Unique source identifier from gaia_source
+        number_mp (long): Minor Planet number attributed by MPC
+        denomination (string): Name of asteroid in MPC database
+        num_of_obs (int): Number of observations for the asteroid 
+            that appear in sso_observation
+        observations (dataframe): Dataframe of observations from sso_observation. 
+            Includes source_id, number_mp, epoch, ra, dec.
+    """
     def __init__(self, source_id=0, number_mp=0, denomination=''):
         self.source_id = source_id
+        self.num_of_obs = 0
         self.number_mp = number_mp
         self.denomination = denomination
-        self.num_of_obs = 0
-        self.epoch = np.array([])
-        self.ra = np.array([])
-        self.dec = np.array([])
+        self.source = astropy.table.Table()
+        self.observations = astropy.table.Table()
     
     def set_sso_source(self, sso_source_results):
+        """
+        Sets attributes of Asteroid object from results of Gaia query in sso_source
+
+        Args:
+            sso_source_results (Table): Table of results from query job for gaia.sso_source.
+                Includes source_id, num_of_obs, number_mp, denomination.
+        
+        Returns:
+            Table: Table of results from query job including source_id, num_of_obs, number_mp, denomination
+        """
         self.source_id = sso_source_results['source_id']
         self.num_of_obs = sso_source_results['num_of_obs']
         self.number_mp = sso_source_results['number_mp']
         self.denomination = sso_source_results['denomination']
+        self.source = sso_source_results
+        
+        return self.source
 
     def get_sso_source_from_source_id(self):
         query = """SELECT
@@ -59,10 +90,10 @@ class Asteroid(object):
 
     def get_sso_observation_from_number_mp(self):
         query = """SELECT
-                source_id, num_of_obs, number_mp, denomination
-                FROM gaiadr2.sso_source
-                WHERE denomination={denomination}
-                """.format(denomination=self.denomination)
+                source_id, number_mp, epoch, ra, dec
+                FROM gaiadr2.sso_observation
+                WHERE number_mp={number_mp}
+                """.format(number_mp=self.number_mp)
 
         job = Gaia.launch_job(query)
         results = job.get_results()
